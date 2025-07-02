2e1df2d1c3cbc904aac9649fdb8c007f73cb84d3
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-07-02 13:13:29 +0200

    [upd] inserimento commenti per ordine

--- js/script.js
+++ js/script.js
@@ -4,9 +4,20 @@
 //logica
 
 for (let i = 1; i <= 100; i++) {
+  // controllo se è multiplo di 3 e 5
   if (i % 3 === 0 && i % 5 === 0) {
     console.log(`FizzBuzz`);
-  } else if (i % 3 === 0) {
+  }
+  // controllo se è mulitplo SOLO di 3 
+  else if (i % 3 === 0) {
     console.log(`Fizz`);
+  }
+  // controllo se è mulitplo SOLO di 5
+  else if (i % 5 === 0) {
+    console.log(`Buzz`);
+  }
+  //altrimenti stampo i
+  else {
+    console.log(i);
   }
 }
